ad2001658f0e065bccc02c7fb55ab6ac72a8fd48
jyoti-wp <sim_s006@icloud.com>
2021-01-31 21:40:09 +0530

    footer icon and link update

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,17 +2,18 @@
 
 function Footer() {
     return (
-        <div class="flex flex-wrap -mx-1 overflow-hidden">
-            <div class="my-1 px-1 w-full overflow-hidden sm:w-full xl:w-1/3">
+        <div className="flex flex-wrap -mx-1 overflow-hidden">
+            <div className="my-1 px-1 w-full overflow-hidden sm:w-full xl:w-1/3">
                  dummy text
                 {/* column Content */}
             </div>
-            <div class="my-1 px-1 w-full overflow-hidden sm:w-full xl:w-1/3">
+            <div className="my-1 px-1 w-full overflow-hidden sm:w-full xl:w-1/3">
             dummy text
                 {/* <!-- Column Content --> */}
             </div>
-            <div class="my-1 px-1 w-full overflow-hidden sm:w-full xl:w-1/3">
+            <div className="my-1 px-1 w-full overflow-hidden sm:w-full xl:w-1/3">
             dummy text
+            <h1 className="bg-black md:bg-gray-600 xl:bg-pink-600">Hello India</h1>
                 {/* <!-- Column Content --> */}
             </div>
         </div>
