67a13cd692f987b4f9dc44a9823039b096a9698c
jyoti-wp <sim_s006@icloud.com>
2021-02-18 00:42:01 +0530

    footer link update

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,9 +8,6 @@ function Footer() {
                 <div className="lg:w-1/4 md:w-1/2 w-full px-4">
                     <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
                     <nav className="list-none mb-10">
-                    <LINK href="">
-                        <a className="text-gray-600 hover:text-gray-800">My Work</a>
-                    </LINK>
                     <li>
                         <a className="text-gray-600 hover:text-gray-800">About Us</a>
                     </li>
